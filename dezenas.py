78de7670fc6bda1cd21d87b45aa90514f5987b4b
cassKirst <37675576+cassKirst@users.noreply.github.com>
2019-06-06 22:21:23 -0300

    Add files via upload

---
+++ dezenas.py
@@ -0,0 +1,8 @@
+entrada = int(input("Digite um número inteiro:"))
+
+apoio = entrada % 100
+
+dezena = apoio // 10
+
+
+print("O dígito das dezenas é",dezena)
---
+++ segundos.py
@@ -0,0 +1,13 @@
+
+
+total_segs = int(input("Por favor, entre com o número de segundos que deseja converter:"))
+
+dias = total_segs // 86400
+segs_restantes = total_segs % 86400
+horas = segs_restantes // 3600
+segs_restantes = segs_restantes % 3600
+minutos =  segs_restantes // 60
+segs_restantes_final = segs_restantes % 60
+
+
+print(dias,"dias,",horas,"horas,",minutos,"minutos e",segs_restantes_final,"segundos.")
